052079360e33d311dad9db8c7013de35159f28b9
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-22 00:03:24 +0200

    done BUT I have to add a readme

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-const photos = Array.from(document.querySelectorAll("#picture"));
-let activeImage = null;
+const images = document.querySelectorAll(".picture")
 
-const expandImage = (currentPicture => {
-    if (activeImage === currentPicture) {
-        return;
-    }
-    if (activeImage) {
-        activeImage.classList.remove("active")
-    }
-    currentPicture.classList.add("active") 
-    activeImage = currentPicture
-})
-
-
-photos.map((photo) => {
-    photo.addEventListener("click", (e) => {
-        expandImage(e.target)
+images.forEach((picture) => {
+    picture.addEventListener("click", () => {
+        removeActive()
+        picture.classList.add("active")
     })
 })
 
+function removeActive() {
+    images.forEach((picture) => {
+        picture.classList.remove("active")
+    })
+}

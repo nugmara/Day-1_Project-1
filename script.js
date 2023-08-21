63a6ce674253d1a11f4c38137fe16ae83f1d4582
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-21 20:27:24 +0200

    Create the effect of expanding the pictures

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function myFunction() {
-    const image = document.getElementById("picture")
-    image.classList.add("active");
-}
+const photos = Array.from(document.querySelectorAll("#picture"));
+let activeImage = null;
+
+const expandImage = (currentPicture => {
+    if (activeImage === currentPicture) {
+        return;
+    }
+    if (activeImage) {
+        activeImage.classList.remove("active")
+    }
+    currentPicture.classList.add("active") 
+    activeImage = currentPicture
+})
+
+
+photos.map((photo) => {
+    photo.addEventListener("click", (e) => {
+        expandImage(e.target)
+    })
+})
+
